1eef748d5eaf5eaf9b6f57efabbf41b5793c7c23
SPaulEmanuel <paul.smocot@assist.ro>
2023-10-25 16:54:48 +0300

    Fix implementation

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Button, Group, useMantineColorScheme } from "@mantine/core";
+import React from "react"
+
+export default function HomePage() {
+  const { setColorScheme, clearColorScheme } = useMantineColorScheme();
+
+  return (
+    <Group>
+      <Button onClick={() => setColorScheme('light')}>Light</Button>
+      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
+      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      <Button onClick={clearColorScheme}>Clear</Button>
+    </Group>
+  );
+}
